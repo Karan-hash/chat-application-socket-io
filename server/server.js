c1af9dbed2329f289cd082380790764af465e73d
karan00033 <karan.1123@zenmonk.tech>
2023-11-07 17:31:07 +0530

    Socket IO server side code done

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 const connectDatabase = require("./db/Database");
+
+// const socketIo = require("socket.io");
 // const cloudinary = require("cloudinary");
 
 // Handling uncaught Exception
@@ -20,7 +22,7 @@ connectDatabase();
 
 
 app.get("/", (req, res) => {
-    res.send("Chat Application Backend apis are running succesfully");
+  res.send("Chat Application Backend apis are running succesfully");
 })
 
 const PORT = process.env.PORT || 5000;
@@ -30,6 +32,52 @@ const server = app.listen(PORT, () => {
     `Server is running on http://localhost:${process.env.PORT}`
   );
 });
+
+const ioinstance = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
+
+ioinstance.on("connection", (socket) => {
+  console.log("A user connected to socket.io");
+
+  // Creating connection and then creating the new room
+  socket.on('setup', (userData) => {
+    // Creating new room with the id of the user _id 
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  // Joining the chat
+  socket.on("Joining the chat", (room) => {
+    socket.join(room);
+    console.log("User Joined the room: " + room);
+  });
+
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing")
+  }
+  );
+  socket.on("stop typing", (room) => { socket.in(room).emit("stop typing") });
+
+  socket.on("new message", (newMessageRecieved) => {
+    let message = newMessageRecieved.chat;
+    if (!message.users) return console.log("chat.users not defined");
+    message.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("New message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+})
 
 // unhandled promise rejection
 process.on("unhandledRejection", (err) => {
